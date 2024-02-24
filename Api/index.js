8843d3a4ca799f1d5b204b1f7c6e1d6e06355871
Sachin Uthpala <gunasekara.s.u.d.a@gmail.com>
2024-02-24 12:05:39 +0530

    connect to the database

--- Api/index.js
+++ Api/index.js
@@ -1,8 +1,18 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 const rPort = 3000
+const ConnetMongoDB = process.env.MODB
+
+mongoose.connect(ConnetMongoDB ).then(() => {
+    console.log('DB Connection is Sucessfull 🌍♨️♨️♨️')
+});
+
 
 app.listen ( rPort , () => {
-    console.log(`Server Is Running on Port Number : ${rPort} Sucessfully..😍😍😍🌍`);
+    console.log(`Server Is Running on Port Number : ${rPort} Sucessfully..😍🌍`);
 })
